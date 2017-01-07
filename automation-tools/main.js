a80270870c22e18ff48f75085e377493e67133ec
Carmen Liu <carmen.liujia@gmail.com>
2017-01-07 17:37:06 +1300

    console log with color

--- automation-tools/main.js
+++ automation-tools/main.js
@@ -1,6 +1,6 @@
 var casper = require('casper').create();
-console.log('hello world');
+// console.log('hello world');
+// casper.echo('hello world', 'INFO');
+casper.echo('hello world', 'ERROR');
 //tell terminal to exit
 casper.exit();
-
-
