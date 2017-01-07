9eaa08f1c81c3391ad92328babbd536389927d63
Carmen Liu <carmen.liujia@gmail.com>
2017-01-08 10:53:05 +1300

    using repeat method to capture images with differ size

--- automation-tools/main2.js
+++ automation-tools/main2.js
@@ -6,17 +6,25 @@ var viewportSizes = [480,720,1200];
 // console.log(viewportSizes);
 
 casper.start();
-casper.each(urls, function(self, items, index){
-  console.log(self,items, index);
-  self.thenOpen(items, function() {
-    var title = this.getTitle();
-    console.log(title);
-    this.wait(2000, function(){
-      this.capture('./images/screenshot_' + index + '.png');
-    });
 
+var counter = 0;
+
+casper.repeat(viewportSizes.length, function(){
+  var viewportSize = viewportSizes[counter];
+  // console.log(viewportSize);
+  casper.viewport(viewportSize,1000).each(urls, function(self, items, index){
+    console.log(self,items, index);
+    self.thenOpen(items, function() {
+      var title = this.getTitle();
+      console.log(title);
+      this.wait(2000, function(){
+        this.capture('./images/screenshot_' + index +'_'+ viewportSize + '.png');
+      });
+    });
   });
+  counter += 1;
 });
+
 
 
 casper.run();
