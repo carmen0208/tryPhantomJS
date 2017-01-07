ddfe51b25d9384a89ff46abe54cfa45aca46b6b5
Carmen Liu <carmen.liujia@gmail.com>
2017-01-07 20:51:37 +1300

    get title and show it to commandline

--- automation-tools/main.js
+++ automation-tools/main.js
@@ -5,9 +5,15 @@ var casper = require('casper').create();
 // //tell terminal to exit
 // casper.exit();
 casper.start('http://www.google.co.nz/', function(){
-  this.capture('./output/test.png');
+  // this.capture('./output/test.png');
+  var message = "this current page title is :"
+  var title = this.evaluate(function(message){
+    var title = document.title;
+    return message + title;
+  },message);
+  console.log(title);
 });
-casper.thenOpen('http://www.bing.com/',function(){
-  this.capture('./output/test_02.png');
-});
+// casper.thenOpen('http://www.bing.com/',function(){
+//   this.capture('./output/test_02.png');
+// });
 casper.run();
