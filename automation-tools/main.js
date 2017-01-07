284bbe3df4da1f5a5f22ddd5eeadd69383d0c99a
Carmen Liu <carmen.liujia@gmail.com>
2017-01-07 17:43:21 +1300

    how to capture screenshot

--- automation-tools/main.js
+++ automation-tools/main.js
@@ -1,6 +1,13 @@
 var casper = require('casper').create();
 // console.log('hello world');
 // casper.echo('hello world', 'INFO');
-casper.echo('hello world', 'ERROR');
-//tell terminal to exit
-casper.exit();
+// casper.echo('hello world', 'ERROR');
+// //tell terminal to exit
+// casper.exit();
+casper.start('http://www.google.co.nz/', function(){
+  this.capture('./output/test.png');
+});
+casper.thenOpen('http://www.bing.com/',function(){
+  this.capture('./output/test_02.png');
+});
+casper.run();
