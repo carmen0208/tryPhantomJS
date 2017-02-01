f63a4cee3d23e3d0bc021fa3b8125ca660566e73
carmen0208 <carmen.liujia@gmail.com>
2017-02-02 11:07:43 +1300

    getting a idea of how to run test with CasperJS

--- automation-tools/README.md
+++ automation-tools/README.md
@@ -1,4 +1,10 @@
-# How to run 
+# How to run
 ```sh
 npm start
+```
+
+# How to run test with CasperJS
+
+```sh
+npm test
 ```
--- automation-tools/test/main.js
+++ automation-tools/test/main.js
@@ -1,11 +1,40 @@
 var url = 'https://duckduckgo.com/';
 
+function checkSelectorForAttr(selector, attr) {
+  var results = [];
+  var elements = document.querySelectorAll(selector);
+  if(elements.length == 0) {
+    return null;
+  }
+  for (var i=0;i<elements.length;i++) {
+    var current = elements[i];
+    var hasAttr = current.hasAttribute(attr);
+    if(!hasAttr) {
+      results.push(current.outerHTML);
+    }
+  }
+  return results;
+}
+
 casper.test.begin('Testing the status', function(test) {
   casper.start(url,function(){
-    test.assertHttpStatus(200,'Page is up and running');
+    // test.assertHttpStatus(200,'Page is up and running');
   });
   casper.then(function(){
-    test.assert(casper.getCurrentUrl() === url, 'URL is the one expected');
+    // test.assert(casper.getCurrentUrl() === url, 'URL is the one expected');
+    test.assertExists('html[lang]', 'A html element with a "lang" attribute exists');
+    test.assertTruthy(this.getElementAttribute('html[lang]', 'lang'),'html lang attribute has a value');
+  });
+
+  casper.then(function() {
+    test.assertExists('head title', 'A title element exists inside the head');
+  });
+
+  casper.then(function() {
+    var imageWithNoAltAttr = this.evaluate(checkSelectorForAttr, 'img','alt');
+    if(imageWithNoAltAttr && imageWithNoAltAttr.length > 0) {
+      test.fail('Some image don\'t have an "alt" attribute');
+    }
   });
 
   casper.run(function(){
